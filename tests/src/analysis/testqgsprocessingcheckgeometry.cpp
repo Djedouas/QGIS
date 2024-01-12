abb3c77cb6796e7d5593894a10f086bcf52ff614
Jacky Volpes <jacky.volpes@oslandia.com>
2024-01-12 12:08:38 +0100

    Processing: Add Check Geometry algorithm and test: Line layer intersection

--- tests/src/analysis/testqgsprocessingcheckgeometry.cpp
+++ tests/src/analysis/testqgsprocessingcheckgeometry.cpp
@@ -32,6 +32,8 @@ class TestQgsProcessingCheckGeometry: public QgsTest
     void init() {} // will be called before each testfunction is executed.
     void cleanup() {} // will be called after every testfunction.
 
+    void lineLayerIntersectionAlg();
+
     void angleAlg_data();
     void angleAlg();
 
@@ -111,6 +113,35 @@ void TestQgsProcessingCheckGeometry::angleAlg()
   QVERIFY( errorsLayer->isValid() );
   QCOMPARE( outputLayer->featureCount(), expectedErrorCount );
   QCOMPARE( errorsLayer->featureCount(), expectedErrorCount );
+}
+
+void TestQgsProcessingCheckGeometry::lineLayerIntersectionAlg()
+{
+  std::unique_ptr< QgsProcessingAlgorithm > alg(
+    QgsApplication::processingRegistry()->createAlgorithmById( QStringLiteral( "native:checkgeometrylinelayerintersection" ) )
+  );
+  QVERIFY( alg != nullptr );
+
+  QVariantMap parameters;
+  parameters.insert( QStringLiteral( "INPUT" ), QVariant::fromValue( mLineLayer ) );
+  parameters.insert( QStringLiteral( "INTERSECTION_LAYER" ), QVariant::fromValue( mPolygonLayer ) );
+  parameters.insert( QStringLiteral( "OUTPUT" ), QgsProcessing::TEMPORARY_OUTPUT );
+  parameters.insert( QStringLiteral( "ERRORS" ), QgsProcessing::TEMPORARY_OUTPUT );
+
+  bool ok = false;
+  QgsProcessingFeedback feedback;
+  std::unique_ptr< QgsProcessingContext > context = std::make_unique< QgsProcessingContext >();
+
+  QVariantMap results;
+  results = alg->run( parameters, *context, &feedback, &ok );
+  QVERIFY( ok );
+
+  std::unique_ptr<QgsVectorLayer> outputLayer( qobject_cast< QgsVectorLayer * >( context->getMapLayer( results.value( QStringLiteral( "OUTPUT" ) ).toString() ) ) );
+  std::unique_ptr<QgsVectorLayer> errorsLayer( qobject_cast< QgsVectorLayer * >( context->getMapLayer( results.value( QStringLiteral( "ERRORS" ) ).toString() ) ) );
+  QVERIFY( outputLayer->isValid() );
+  QVERIFY( errorsLayer->isValid() );
+  QCOMPARE( outputLayer->featureCount(), 5 );
+  QCOMPARE( errorsLayer->featureCount(), 5 );
 }
 
 QGSTEST_MAIN( TestQgsProcessingCheckGeometry )
