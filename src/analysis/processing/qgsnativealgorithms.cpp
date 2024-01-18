16acb069971b850f3281841ed58633884f14419d
Jacky Volpes <jacky.volpes@oslandia.com>
2024-01-18 18:21:16 +0100

    Processing: Add Fix Geometry algorithm and test: Angle
    Porting angle fix from geometry checker to processings

--- src/analysis/processing/qgsnativealgorithms.cpp
+++ src/analysis/processing/qgsnativealgorithms.cpp
@@ -237,6 +237,7 @@
 #include "qgsalgorithmzonalstatistics.h"
 #include "qgsalgorithmzonalstatisticsfeaturebased.h"
 #include "qgsalgorithmpolygonstolines.h"
+#include "qgsalgorithmfixgeometryangle.h"
 #include "qgsbookmarkalgorithms.h"
 #include "qgsprojectstylealgorithms.h"
 #include "qgsstylealgorithms.h"
@@ -557,6 +558,7 @@ void QgsNativeAlgorithms::loadAlgorithms()
   addAlgorithm( new QgsPolygonsToLinesAlgorithm() );
   addAlgorithm( new QgsDensifyGeometriesByIntervalAlgorithm() );
   addAlgorithm( new QgsDensifyGeometriesByCountAlgorithm() );
+  addAlgorithm( new QgsFixGeometryAngleAlgorithm() );
 }
 
 ///@endcond
