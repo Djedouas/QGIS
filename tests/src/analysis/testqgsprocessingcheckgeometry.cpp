7e9cf0af1561129e58aa77085785bc7ebc3f33a2
Jacky Volpes <jacky.volpes@oslandia.com>
2024-02-21 17:21:59 +0100

    Processing: Add Check Geometry algorithm and test: Duplicate Nodes

--- tests/src/analysis/testqgsprocessingcheckgeometry.cpp
+++ tests/src/analysis/testqgsprocessingcheckgeometry.cpp
@@ -35,6 +35,9 @@ class TestQgsProcessingCheckGeometry: public QgsTest
     void angleAlg_data();
     void angleAlg();
 
+    void duplicateNodesAlg_data();
+    void duplicateNodesAlg();
+
   private:
 
     QgsVectorLayer *mLineLayer = nullptr;
@@ -94,6 +97,45 @@ void TestQgsProcessingCheckGeometry::angleAlg()
   QVariantMap parameters;
   parameters.insert( QStringLiteral( "INPUT" ), QVariant::fromValue( layerToTest ) );
   parameters.insert( QStringLiteral( "MIN_ANGLE" ), 15 );
+  parameters.insert( QStringLiteral( "OUTPUT" ), QgsProcessing::TEMPORARY_OUTPUT );
+  parameters.insert( QStringLiteral( "ERRORS" ), QgsProcessing::TEMPORARY_OUTPUT );
+
+  bool ok = false;
+  QgsProcessingFeedback feedback;
+  std::unique_ptr< QgsProcessingContext > context = std::make_unique< QgsProcessingContext >();
+
+  QVariantMap results;
+  results = alg->run( parameters, *context, &feedback, &ok );
+  QVERIFY( ok );
+
+  std::unique_ptr<QgsVectorLayer> outputLayer( qobject_cast< QgsVectorLayer * >( context->getMapLayer( results.value( QStringLiteral( "OUTPUT" ) ).toString() ) ) );
+  std::unique_ptr<QgsVectorLayer> errorsLayer( qobject_cast< QgsVectorLayer * >( context->getMapLayer( results.value( QStringLiteral( "ERRORS" ) ).toString() ) ) );
+  QVERIFY( outputLayer->isValid() );
+  QVERIFY( errorsLayer->isValid() );
+  QCOMPARE( outputLayer->featureCount(), expectedErrorCount );
+  QCOMPARE( errorsLayer->featureCount(), expectedErrorCount );
+}
+
+void TestQgsProcessingCheckGeometry::duplicateNodesAlg_data()
+{
+  QTest::addColumn<QgsVectorLayer *>( "layerToTest" );
+  QTest::addColumn<int>( "expectedErrorCount" );
+  QTest::newRow( "Line layer" ) << mLineLayer << 3;
+  QTest::newRow( "Polygon layer" ) << mPolygonLayer << 1;
+}
+
+void TestQgsProcessingCheckGeometry::duplicateNodesAlg()
+{
+  QFETCH( QgsVectorLayer *, layerToTest );
+  QFETCH( int, expectedErrorCount );
+
+  std::unique_ptr< QgsProcessingAlgorithm > alg(
+    QgsApplication::processingRegistry()->createAlgorithmById( QStringLiteral( "native:checkgeometryduplicatenodes" ) )
+  );
+  QVERIFY( alg != nullptr );
+
+  QVariantMap parameters;
+  parameters.insert( QStringLiteral( "INPUT" ), QVariant::fromValue( layerToTest ) );
   parameters.insert( QStringLiteral( "OUTPUT" ), QgsProcessing::TEMPORARY_OUTPUT );
   parameters.insert( QStringLiteral( "ERRORS" ), QgsProcessing::TEMPORARY_OUTPUT );
 
